5907989938706e7279a0c0721ba67e4d97707ed8
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-04-15 00:39:46 +0300

    about added

---
+++ src/components/auth/RegisterForm.jsx
@@ -0,0 +1,143 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { Form } from "formik";
+import { object, string } from "yup";
+
+import { useState } from "react";
+import { FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+
+export const registerSchema = object({
+  username: string()
+    .max(10, "Kullanıcı adı 10 karakterden az olmalıdır.")
+    .required(),
+
+  email: string().email().required(),
+  password: string()
+    .required("password zorunludur")
+    .min(8, "password en az 8 karakter olmalıdır")
+    .max(20, "password en fazla 20 karakter olmalıdır")
+    .matches(/\d+/, "Password bir sayı içermelidir")
+    .matches(/[a-z]/, "Password bir küçük harf içermelidir")
+    .matches(/[A-Z]/, "Password bir büyük harf içermelidir")
+    .matches(/[!,?{}><%&$#£+-.]+/, "Password bir özel karakter içermelidir"),
+});
+const RegisterForm = ({
+  values,
+  handleChange,
+  errors,
+  touched,
+  handleBlur,
+}) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+      };
+  return (
+    <div>
+      <Form>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+          <TextField
+            label="User Name"
+            name="username"
+            id="userName"
+            type="text"
+            variant="outlined"
+            value={values.username}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            helperText={touched.username && errors.username}
+            error={touched.username && Boolean(errors.username)}
+          />
+          <TextField
+            label="Email"
+            name="email"
+            id="email"
+            type="email"
+            variant="outlined"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            helperText={touched.email && errors.email}
+            error={touched.email && Boolean(errors.email)}
+          />
+          <TextField
+            label="Image"
+            name="image"
+            id="image"
+            type="url"
+            variant="outlined"
+            value={values.image}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            helperText={touched.image && errors.image}
+            error={touched.image && Boolean(errors.image)}
+          />
+          <TextField
+            label="Bio"
+            name="bio"
+            id="bio"
+            type="text"
+            variant="outlined"
+            value={values.bio}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            helperText={touched.bio && errors.bio}
+            error={touched.bio && Boolean(errors.bio)}
+          />
+
+          {/* <TextField
+            label="password"
+            name="password"
+            id="password"
+            type="password"
+            variant="outlined"
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            helperText={touched.password && errors.password}
+            error={touched.password && Boolean(errors.password)}
+           
+         / > */}
+         
+            <OutlinedInput
+            name="password"
+            label="password"
+              id="password"
+              variant="outlined"
+              type={showPassword ? "text" : "password"}
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              helperText={touched.password && errors.password}
+              error={touched.password && Boolean(errors.password)}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                    
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            
+            />
+   
+
+          <Button type="submit" variant="contained" size="large">
+            Submit
+          </Button>
+        </Box>
+      </Form>
+    </div>
+  );
+};
+export default RegisterForm;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,112 @@
+import { Box, Paper, Typography } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import InstagramIcon from "@mui/icons-material/Instagram";
+const iconLinkdn = {
+  fontSize:"50px",
+  color:"black",
+  margin:"1rem",
+  "&:hover":{
+      color:"blue",
+     
+  }
+}
+const iconTwtr = {
+  fontSize:"50px",
+  color:"black",
+  margin:"1rem",
+  "&:hover":{
+      color:"#00acee",
+      
+  }
+}
+const iconIns = {
+  fontSize:"50px",
+  color:"black",
+  margin:"1rem",
+  "&:hover":{
+      color:"#E1306C",
+  
+  }
+}
+const iconYou = {
+  fontSize:"50px",
+  color:"black",
+  margin:"1rem",
+  "&:hover":{
+      color:"red",
+   
+  }
+}
+const iconGthb = {
+  fontSize:"50px",
+  color:"black",
+  margin:"1rem",
+  "&:hover":{
+      color:"red",
+      
+  }
+}
 
 const About = () => {
   return (
-    <div>
-      about
-    </div>
+    <Box
+    sx={{
+      width: "100%",
+      height: "94vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+     
+    }}
+    
+    >
+    <Box
+      sx={{
+        width: "500px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      
+      }}
+    
+    >
+      <Paper elevation={12} sx={{ borderRadius:"20px"}}>
+        <img src="https://cdn.kibrispdr.org/data/136/emoji-hijab-iphone-48.jpg" width={400}/>
+
+        <Typography sx={{ textAlign: "center", fontSize: "28px" }}>
+          Hümeyra
+        </Typography>
+        <Typography sx={{ textAlign: "center", fontSize: "28px" }}>
+          Frontend Developer
+        </Typography>
+        <Box>
+          <Link target="blank" to="">
+           <LinkedInIcon sx={iconLinkdn}/>
+          </Link>
+          <Link target="blank" to="">
+           <GitHubIcon sx={iconGthb}/>
+          </Link>
+          <Link target="blank" to="">
+           <TwitterIcon sx={iconTwtr}/>
+          </Link>
+          <Link target="blank" to="">
+           <InstagramIcon sx={iconIns}/>
+          </Link>
+          <Link target="blank" to="https://www.youtube.com/channel/UChPOg76pva-S9q9ZYxII08g">
+           <YouTubeIcon sx={iconYou}/>
+          </Link>
+
+
+        </Box>
+
+      </Paper>
+    </Box>
+    </Box>
   )
 }
 
