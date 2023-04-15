e9a4441b8904843c8a8c78338ff042561146fb56
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-04-15 14:07:15 +0300

    blogslice updated

--- src/features/blogSlice.jsx
+++ src/features/blogSlice.jsx
@@ -1,35 +1,40 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const blogSlice = createSlice({
-  name: "stock",
+  name: "blog",
 
   initialState: {
-    // purchases: [],
-    // sales: [],
-    // brands: [],
-    // firms: [],
-    // products: [],
-    // categories: [],
+    blogs: [],
+    categories: [],
+    likes: [],
+    comments: [],
+    details: [],    
+    myBlog: [],
     loading: false,
     error: false,
   },
+
   reducers: {
     fetchStart: (state) => {
       state.loading = true;
       state.error = false;
     },
-    getSuccess: (state, { payload:{data,url} }) => {
-      state.loading = false;
-      state[url] = data
-    
+    getBlogSuccess: (state, { payload: { data, url } }) => {
+      state.loading = false; 
+      state[url] = data    
+    },  
+    getMyBlogSuccess: (state, { payload: { data } }) => {
+      state.loading = true;
+      state.myBlog = data 
     },
-    getProCatBrandSuccess: (state, { payload }) => {
-      state.loading = false
-      state.products = payload[0]
-      state.categories = payload[1]
-      state.brands = payload[2]
+    getDetailSuccess: (state, { payload:{ data } }) => {
+      state.loading = true;
+      state.details = data 
     },
-    
+    getCommentsSuccess: (state, { payload: { data } }) => {
+      state.loading = true;
+      state.comments = data 
+    },
     fetchFail: (state) => {
       state.loading = false;
       state.error = true;
@@ -39,8 +44,10 @@ const blogSlice = createSlice({
 
 export const {
   fetchStart,
-  getSuccess,
-  getProCatBrandSuccess,
+  getBlogSuccess,
+  getMyBlogSuccess,
+  getDetailSuccess,
+  getCommentsSuccess,
   fetchFail,
 } = blogSlice.actions;
 export default blogSlice.reducer;
