34905e30b924346b06846072408990fbe0f2e333
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-04-15 14:11:46 +0300

    blogslice added

---
+++ src/hooks/useBlogCalls.jsx
@@ -0,0 +1,115 @@
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchFail,
+  getBlogSuccess,
+  getMyBlogSuccess,
+  getDetailSuccess,
+  getCommentsSuccess,
+  fetchStart,
+  
+} from "../features/stockSlice";
+import useAxios from "./useAxios";
+import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
+
+const useBlogCall = () => {
+  const { token } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const { axiosPublic, axiosWithToken } = useAxios();
+
+
+  const getBlogData = async (url) => {
+    dispatch(fetchStart());
+    try {
+      const { data } = await axiosPublic.get(`api/${url}/`);
+      dispatch(getBlogSuccess({ data, url }));     
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchFail());
+    }
+  };
+   
+  const getMyBlogData = async (url, id) => {    
+    dispatch(fetchStart());
+    try {     
+      await axiosWithToken.get(`api/${url}/${id}/`);
+      dispatch(getBlogSuccess({ data, url }));
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchFail());
+    }
+  };
+
+  const getDetailData = async (url, id) => {    
+    dispatch(fetchStart());
+    try {     
+      await axiosWithToken.get(`api/${url}/${id}/`);
+      dispatch(getDetailSuccess({ data, url }));
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchFail());
+    }
+  };
+
+  const getCommentData = async (url, id) => {    
+    dispatch(fetchStart());
+    try {     
+      await axiosWithToken.get(`api/${url}/${id}/`);
+      dispatch(getCommentsSuccess({ data, url }));
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchFail());
+    }
+  };
+
+  const deleteBlogData = async (url, id) => {    
+    dispatch(fetchStart());
+    try {     
+      await axiosWithToken.delete(`api/${url}/${id}/`);
+      getBlogData(url);
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchFail());
+    }
+  };
+  
+  const postBlogData = async (url, info) => {
+    dispatch(fetchStart());
+    try {
+      await axiosWithToken.post(`api/${url}/`, info);
+      toastSuccessNotify(`${url} successfuly posted`);
+      getBlogData(url);
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchFail());
+      toastErrorNotify(`${url} can not be posted`);
+    }
+  };
+  
+  const putBlogData = async (url, info) => {
+    dispatch(fetchStart());
+    try {
+      await axiosWithToken.put(`api/${url}/${info.id}/`, info);
+      toastSuccessNotify(`${url} successfuly updated`);
+      getBlogData(url);
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchFail());
+      toastErrorNotify(`${url} can not be updated`);
+    }
+  };
+
+
+  return {
+    getBlogData,
+    getMyBlogData,
+    getDetailData,
+    getCommentData,
+    deleteBlogData,
+    postBlogData,
+    putBlogData,
+   
+  };
+};
+
+export default useBlogCall;
